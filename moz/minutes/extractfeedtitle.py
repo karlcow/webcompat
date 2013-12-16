9b93d9b5355f9d98e1c4f0cc1508a38b8e821c60
karl <karl+github@la-grange.net>
2013-12-16 12:06:37 -0500

    fixing a few things andcleaning

--- moz/minutes/extractfeedtitle.py
+++ moz/minutes/extractfeedtitle.py
@@ -11,14 +11,10 @@ MIT License
 import feedparser
 from datetime import date
 from datetime import datetime
-from datetime import timedelta
 
 URL = 'http://planet.webcompat.com/atom.xml'
 today = date.today()
 todayobj = datetime.combine(today, datetime.min.time())
-week = timedelta(days=7)
-# datetime.combine(today, datetime.min.time())
-# print today.isoformat()
 
 feed = feedparser.parse(URL)
 for item in feed['items']:
@@ -26,7 +22,7 @@ for item in feed['items']:
     shortdate = postdate[0:10]
     dateobj = datetime.strptime(postdate, '%Y-%m-%dT%H:%M:%SZ')
     publishedago = todayobj-dateobj
-    if publishedago.days+1 > 8:
+    if publishedago.days+1 < 8:
         postlink = item['link']
         title = item['title']
         title = title.replace('[', '(')
