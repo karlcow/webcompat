d9d702b63f9549ff8f39c0e27936d2e1cd98ece5
Karl Dubost <karl+github@la-grange.net>
2014-09-25 13:46:51 +0900

    fetching the content from etherpad

--- moz/minutes/minutes.py
+++ moz/minutes/minutes.py
@@ -16,12 +16,21 @@ https://etherpad.mozilla.org/ep/pad/export/webcompat/latest?format=txt
 4. Convert the text to the appropriate format
 """
 
+import requests
 import sys
 WIKI_TEMPLATE = '''== Minutes =='''
 URL = 'https://etherpad.mozilla.org/ep/pad/export/webcompat/latest?format=txt'
 SERVER_URL = 'https://etherpad.mozilla.org'
+def etherpad_content(server_uri, pad_name, pad_format='txt'):
+    '''Fetch the text version of the etherpad.'''
+    url = '{0}/ep/pad/export/{1}/latest?format={2}'.format(server_uri,
+                                                           pad_name,
+                                                           pad_format)
+    content = requests.get(url)
+    return content.text
 def main():
     '''core program'''
     # Fetch the content online
+    raw_content = etherpad_content(SERVER_URL, 'webcompat', 'txt')
 if __name__ == "__main__":
     sys.exit(main())
