7bb0da85e17872051934ca1d9e9c0cc904da723c
Karl Dubost <karl+github@la-grange.net>
2014-09-25 13:46:04 +0900

    Script for extracting the minutes automatically

---
+++ moz/minutes/minutes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+minutes.py
+
+Created by Karl Dubost on 2014-09-25.
+Copyright (c) 2014 La Grange. All rights reserved.
+MIT License
+
+We want:
+
+1. Import
+https://etherpad.mozilla.org/ep/pad/export/webcompat/latest?format=txt
+2. Extract the effective minutes from the text
+3. Extract the date
+4. Convert the text to the appropriate format
+"""
+
+import sys
+WIKI_TEMPLATE = '''== Minutes =='''
+URL = 'https://etherpad.mozilla.org/ep/pad/export/webcompat/latest?format=txt'
+SERVER_URL = 'https://etherpad.mozilla.org'
+def main():
+    '''core program'''
+    # Fetch the content online
+if __name__ == "__main__":
+    sys.exit(main())
