87ca8475f58b057e8043f8b398bd76123a89a733
Karl Dubost <karl+github@la-grange.net>
2016-02-24 15:40:09 +0900

    Revert "parsing html"
    This reverts commit 3160c4bb0eb74ba4723ef3a0cadc0e69bfc8c833.

--- moz/minutes/helpers.py
+++ moz/minutes/helpers.py
@@ -9,30 +9,11 @@ Created by Karl Dubost on 2016-02-24.
 Copyright (c) 2016 La Grange. All rights reserved.
 MIT License
 """
-
-import io
-import sys
-
-import lxml.html
 import requests
-
-
 def fetch_content(uri):
     '''Fetch the URI and returns the raw content and its encoding'''
     content = requests.get(uri)
     return content.text, content.encoding
-
-
-def html_parse(content):
-    '''returns a parsed HTML content'''
-    html = ''
-    try:
-        html = lxml.html.parse(io.StringIO(content))
-    except Exception, e:
-        raise e
-    return html
-
-
 def main():
     '''core program'''
     pass
