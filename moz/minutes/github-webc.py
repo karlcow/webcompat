f5185bc3f2557484009e196304fe7618d5b1bb38
Karl Dubost <karl+github@la-grange.net>
2016-02-24 11:27:42 +0900

    code for extracting some data from github

---
+++ moz/minutes/github-webc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+github-webc.py
+Extracts some data from our GitHub project
+
+
+Created by Karl Dubost on 2016-02-24.
+Copyright (c) 2016 La Grange. All rights reserved.
+MIT License
+"""
+
+import sys
+
+
+def main():
+    '''core program'''
+    pass
+
+if __name__ == "__main__":
+    sys.exit(main())
