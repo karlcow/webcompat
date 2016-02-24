9d504575de448cb612376ddc56351993ea972270
Karl Dubost <karl+github@la-grange.net>
2016-02-24 15:37:44 +0900

    for fetching content online

--- moz/minutes/helpers.py
+++ moz/minutes/helpers.py
@@ -9,6 +9,11 @@ Created by Karl Dubost on 2016-02-24.
 Copyright (c) 2016 La Grange. All rights reserved.
 MIT License
 """
+import requests
+def fetch_content(uri):
+    '''Fetch the URI and returns the raw content and its encoding'''
+    content = requests.get(uri)
+    return content.text, content.encoding
 def main():
     '''core program'''
     pass
